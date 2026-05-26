096da8736c512692c5b77a4188e3f6cda7d00e9d
vaishnavikadamcomp2023-collab <vaishnavi.kadam.comp.2023@vpkbiet.org>
2026-05-27 00:10:15 +0530

    fixed render database

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
 class Config:
+
     SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:s%40rthak21@localhost/ngo_db'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
